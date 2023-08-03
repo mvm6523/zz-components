f7646b796a75cf5a0a2016dbe474c2ff272336e1
mvm6523 <240982544@qq.com>
2023-08-03 18:01:18 +0800

    Update transfer.vue

--- packages/components/transfer/src/transfer.vue
+++ packages/components/transfer/src/transfer.vue
@@ -5,34 +5,27 @@
     :modelValue="modelValue"
     @change="change"
   >
-    <template #left-footer>
-      <slot name="left-footer"></slot>
-    </template>
-    <template #right-footer>
-      <slot name="right-footer"></slot>
-    </template>
-    <template #default="{ option }" v-if="haveDefault">
-      <slot :option="option" />
+    <template v-for="itemSlot in Object.keys(slots)" :key="itemSlot" v-slot:[itemSlot]="temp">
+      <slot :name="itemSlot" v-bind="temp"></slot>
     </template>
   </el-transfer>
 </template>
 <script setup lang="ts">
-import { transferProps, transferEmits } from './transfer'
-import { UPDATE_MODEL_EVENT } from '@zz-components/constants'
-import { useSlots } from 'vue'
+import { transferProps, transferEmits } from "./transfer";
+import { UPDATE_MODEL_EVENT } from "@zz-components/constants";
+import { useSlots } from "vue";
 
-const COMPONENT_NAME = 'ZZTransfer'
+const COMPONENT_NAME = "ZZTransfer";
 defineOptions({
-  name: COMPONENT_NAME,
-})
+  name: COMPONENT_NAME
+});
 
-let { options, modelValue } = defineProps(transferProps)
-const emit = defineEmits(transferEmits)
-const changeName = UPDATE_MODEL_EVENT
+let { options, modelValue } = defineProps(transferProps);
+const emit = defineEmits(transferEmits);
+const changeName = UPDATE_MODEL_EVENT;
 
 let change = (value) => {
-  emit(changeName, value)
-}
-let slots = useSlots()
-let haveDefault = slots.default && slots.default()[0].children !== 'v-if'
+  emit(changeName, value);
+};
+let slots = useSlots();
 </script>
